55990cd4559596856c27a0398a8fef96a8254764
yagna <yjt25@email.vccs.edu>
2025-02-03 19:50:25 -0500

    trying to fix project1 go back function

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  base: '/projectmain/', // Change this to match your GitHub Pages repo name
+  base: "/projectmain/", 
   build: {
     outDir: 'docs',
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'), // Main entry point
-        project1: path.resolve(__dirname, 'project1/index.html'), // Additional pages
+        main: path.resolve(__dirname, 'index.html'), 
+        project1: path.resolve(__dirname, 'project1/index.html'), 
         project2: path.resolve(__dirname, 'project2/index.html'), 
       },
     },
