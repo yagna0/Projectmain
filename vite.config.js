98cac953f7f5a7338b85497cf53f759f4bcf3e13
yagna <yjt25@email.vccs.edu>
2025-02-08 23:28:01 -0500

    getting react work on project2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "projectmain", 
+  base: '/projectmain/', 
   build: {
     outDir: "../docs",
     emptyOutDir: true,
